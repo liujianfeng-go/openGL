f84209ec3249e315a965a89a33791e7a2c02ae64
liujianfeng123a <873628264@qq.com>
2023-08-29 22:05:52 +0800

    视频p6提交

---
+++ start-learning-opengl-main/src/02_hello_triangle_re/main.cpp
@@ -0,0 +1,82 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main()
+{
+  glfwInit();
+  // 设置主要和次要版本
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // 创建窗口对象
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  // 设置视口
+  glViewport(0, 0, 800, 600);
+
+  // 注册窗口变化监听
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // 定义顶点数组
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f, // left
+      0.5f, -0.5f, 0.0f,  // right
+      0.0f, 0.5f, 0.0f    // top
+  };
+
+  // 创建顶点缓冲对象
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);           // 生成缓冲对象
+
+  // 绑定缓冲对象
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);  // 绑定缓冲对象到目标上
+
+  // 将顶点数据复制到缓冲对象中
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+
+    // 渲染指令
+    // ...
+    glClearColor(25.0 / 255.0, 25.0 / 255.0, 25.0 / 255.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, 800, 600);
+}
+
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
