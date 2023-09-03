02f4080d5e0d9127c0c4bc8c8c941bd547a6eacc
liujianfeng123a <873628264@qq.com>
2023-09-03 11:41:58 +0800

    修改片段着色器，仅让笑脸图案朝另一个方向看

--- start-learning-opengl-main/src/08_load_texture_exercise_re/shader/fragment.glsl
+++ start-learning-opengl-main/src/08_load_texture_exercise_re/shader/fragment.glsl
@@ -8,5 +8,5 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main(){
-  FracColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
+  FracColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(-TexCoord.x, TexCoord.y)), 0.5);
 }
