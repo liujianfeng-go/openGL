75012207b139875c0533898ac777260ff870c41a
liujianfeng123a <873628264@qq.com>
2023-09-17 21:23:25 +0800

    完善代码

--- start-learning-opengl-main/src/13_model_view_projection_re/main.cpp
+++ start-learning-opengl-main/src/13_model_view_projection_re/main.cpp
@@ -143,19 +143,20 @@ int main(int argc, char *argv[])
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
 
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));                                                       // 观察矩阵 平移 z轴
+    projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f); // 投影矩阵 透视投影
+    ourShader.setMat4("view", view);
+    ourShader.setMat4("projection", projection);
+
     for (unsigned int i = 0; i < 10; i++)
     {
       glm::mat4 model = glm::mat4(1.0f);
-      glm::mat4 view = glm::mat4(1.0f);
-      glm::mat4 projection = glm::mat4(1.0f);
       model = glm::translate(model, cubePositions[i]);
       model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(1.0, 1.0, 1.0));            // 模型矩阵 旋转 55度  x轴                                                       // 模型矩阵 平移 z轴
-      view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));                                                       // 观察矩阵 平移 z轴
-      projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f); // 投影矩阵 透视投影
-
+      
       ourShader.setMat4("model", model);
-      ourShader.setMat4("view", view);
-      ourShader.setMat4("projection", projection);
 
       glBindVertexArray(boxGeometry.VAO);
 
